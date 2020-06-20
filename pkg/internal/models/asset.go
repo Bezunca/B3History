2b1e5d0ceddf68f4c1a18c3863c56b463e71f303
Raphael C. Almeida <raphael.carvalho@e-xyon.com.br>
2020-06-20 20:43:45 -0300

    Added json unmarshal names to AssetInfo struct

--- pkg/internal/models/asset.go
+++ pkg/internal/models/asset.go
@@ -5,30 +5,30 @@ import "time"
 type FixedPoint int // Represents the actual value multiplied by 100
 
 type AssetInfo struct {
-	TipReg                 int
-	DataCollectionDate     time.Time
-	BDICode                int
-	Ticker                 string
-	MarketType             int
-	CompanyName            string
-	SecurityType           string
-	FutureMarketExpiration string // Need to check this against an example that actually has a value
-	Currency               string
-	PriceOpen              FixedPoint
-	PriceMax               FixedPoint
-	PriceMin               FixedPoint
-	PriceMean              FixedPoint
-	PriceClose             FixedPoint
-	PriceBid               FixedPoint
-	PriceAsk               FixedPoint
-	TotalTrades            int
-	TotalQuantity          int
-	TotalVolume            FixedPoint
-	PreExe                 FixedPoint // Needs further investigation
-	IndOpc                 int        // Needs further investigation
-	ExpirationDate         time.Time
-	FatCot                 int // Needs further investigation
-	PtoExe                 int // Needs further investigation
-	ISINCode               string
-	DistributionNumber     int
+	TipReg                 int         	`json:"registration_type"`
+	DataCollectionDate     time.Time   	`json:"data"`
+	BDICode                int         	`json:"bdi_code"`
+	Ticker                 string		`json:"ticker"`
+	MarketType             int			`json:"market_type"`
+	CompanyName            string		`json:"shot_company_name"`
+	SecurityType           string		`json:"security_type"`
+	FutureMarketExpiration string       `json:" future_market_expiration"` // Need to check this against an example that actually has a value
+	Currency               string		`json:"currency"`
+	PriceOpen              FixedPoint	`json:"price_open"`
+	PriceMax               FixedPoint	`json:"price_max"`
+	PriceMin               FixedPoint	`json:"price_min"`
+	PriceMean              FixedPoint	`json:"price_mean"`
+	PriceClose             FixedPoint	`json:"price_close"`
+	PriceBid               FixedPoint	`json:"price_bid"`
+	PriceAsk               FixedPoint	`json:"price_ask"`
+	TotalTrades            int			`json:"total_trades"`
+	TotalQuantity          int			`json:"total_quantity"`
+	TotalVolume            FixedPoint	`json:"total_volume"`
+	PreExe                 FixedPoint   `json:"execution_price"` // Needs further investigation
+	IndOpc                 int        	`json:"options_indicator"`	// Needs further investigation
+	ExpirationDate         time.Time	`json:"expiration_date"`
+	FatCot                 int 			`json:"amount_factor"` // Needs further investigation
+	PtoExe                 int 			`json:"execution_points"` // Needs further investigation
+	ISINCode               string		`json:"isin_code"`
+	DistributionNumber     int			`json:"distribution_number"`
 }
