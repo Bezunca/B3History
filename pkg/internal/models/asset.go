cdf571b4d6eab3f5f036b9a4f1842dc428226011
Raphael C. Almeida <raphael.carvalho@e-xyon.com.br>
2020-06-20 21:25:04 -0300

    Added bson support on data struct

--- pkg/internal/models/asset.go
+++ pkg/internal/models/asset.go
@@ -5,31 +5,31 @@ import "time"
 type FixedPoint int // Represents the actual value multiplied by 100
 
 type AssetInfo struct {
-	Year                   int         	`json:"year"`
-	TipReg                 int         	`json:"-"`
-	DataCollectionDate     time.Time   	`json:"data"`
-	BDICode                int         	`json:"-"`
-	Ticker                 string		`json:"ticker"`
-	MarketType             int			`json:"market_type"`
-	CompanyName            string		`json:"shot_company_name"`
-	SecurityType           string		`json:"security_type"`
-	FutureMarketExpiration string       `json:" future_market_expiration"` // Need to check this against an example that actually has a value
-	Currency               string		`json:"currency"`
-	PriceOpen              FixedPoint	`json:"-"`
-	PriceMax               FixedPoint	`json:"-"`
-	PriceMin               FixedPoint	`json:"-"`
-	PriceMean              FixedPoint	`json:"-"`
-	PriceClose             FixedPoint	`json:"price_close"`
-	PriceBid               FixedPoint	`json:"-"`
-	PriceAsk               FixedPoint	`json:"-"`
-	TotalTrades            int			`json:"-"`
-	TotalQuantity          int			`json:"-"`
-	TotalVolume            FixedPoint	`json:"total_volume"`
-	PreExe                 FixedPoint   `json:"execution_price"` // Needs further investigation
-	IndOpc                 int        	`json:"-"`	// Needs further investigation
-	ExpirationDate         time.Time	`json:"expiration_date"`
-	FatCot                 int 			`json:"-"` // Needs further investigation
-	PtoExe                 int 			`json:"-"` // Needs further investigation
-	ISINCode               string		`json:"-"`
-	DistributionNumber     int			`json:"-"`
+	Year                   int         	`json:"year" bson:"year"`
+	TipReg                 int         	`json:"-" bson:"-" `
+	DataCollectionDate     time.Time   	`json:"data" bson:"data"`
+	BDICode                int         	`json:"-" bson:"-"`
+	Ticker                 string		`json:"ticker" bson:"ticker"`
+	MarketType             int			`json:"market_type" bson:"market_type"`
+	CompanyName            string		`json:"shot_company_name" bson:"shot_company_name"`
+	SecurityType           string		`json:"security_type" bson:"security_type"`
+	FutureMarketExpiration string       `json:" future_market_expiration" bson:" future_market_expiration"` // Need to check this against an example that actually has a value
+	Currency               string		`json:"currency" bson:"currency"`
+	PriceOpen              FixedPoint	`json:"-" bson:"-"`
+	PriceMax               FixedPoint	`json:"-" bson:"-"`
+	PriceMin               FixedPoint	`json:"-" bson:"-"`
+	PriceMean              FixedPoint	`json:"-" bson:"-"`
+	PriceClose             FixedPoint	`json:"price_close" bson:"price_close"`
+	PriceBid               FixedPoint	`json:"-" bson:"-"`
+	PriceAsk               FixedPoint	`json:"-" bson:"-"`
+	TotalTrades            int			`json:"-" bson:"-"`
+	TotalQuantity          int			`json:"-" bson:"-"`
+	TotalVolume            FixedPoint	`json:"total_volume" bson:"total_volume"`
+	PreExe                 FixedPoint   `json:"execution_price" bson:"execution_price"` // Needs further investigation
+	IndOpc                 int        	`json:"-" bson:"-"`	// Needs further investigation
+	ExpirationDate         time.Time	`json:"expiration_date" bson:"expiration_date"`
+	FatCot                 int 			`json:"-" bson:"-"` // Needs further investigation
+	PtoExe                 int 			`json:"-" bson:"-"` // Needs further investigation
+	ISINCode               string		`json:"-" bson:"-"`
+	DistributionNumber     int			`json:"-" bson:"-"`
 }
